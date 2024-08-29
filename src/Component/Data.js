ee6957f03d54dd9f48bf3da9dfa3d5b7851cbd33
Stephanorova <garoust60@gmail.com>
2024-08-28 23:55:35 -1200

    modifier de data

--- src/Component/Data.js
+++ src/Component/Data.js
@@ -1,5 +1,6 @@
 import image from "./Image/image.png"
 import image2 from "./Image/Varotra.PNG"
+import image1 from "./Image/potfolio.png"
 import portfolio from "./Image/Portfolio.PNG"
 import recrute from "./Image/Capture.PNG"
 
@@ -8,15 +9,10 @@ export const data = [
     {
         name : "Porfolio",
         image: portfolio,
-        techno: "Reactjs, Materiel ui, Nodejs, express, strip, Mongodb",
+        techno: "Reactjs, Materiel ui, Email js",
         url: ` https://github.com/Stephanorova/PortFolio`
     },
-    {
-        name : "site Varotra (Nobless E-commerce) ",
-        image: image2,
-        techno: "Reactjs, Materiel ui, TailwindCSS",
-        url: ` https://github.com/Stephanorova/Varotra`
-    },
+ 
     {
         name : "E-recutement en ligne",
         image: recrute,
@@ -24,15 +20,18 @@ export const data = [
         url: ` https://github.com/Stephanorova/E_recrutement_en_ligne`
     },
     {
+        name : "site Varotra (Nobless E-commerce) ",
+        image: image2,
+        techno: "Reactjs, Materiel ui, Nodejs, express, strip, Mongodb",
+        url: ` https://github.com/Stephanorova/Varotra`
+    },
+    {
         name : "Application web pour la gestion de marché",
         image: image,
         techno: "Reactjs, Materiel ui, Nodejs, express, JWT, Mysql"
     },
-    {
-        name : "Porfolio",
-        image: image2,
-        techno: "Reactjs, Materiel ui, TailwindCSS"
-    },
+  
+  
    
    
     
